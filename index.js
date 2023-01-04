5872295bb17e98a362758f49aefd9b06d8490351
mostafaghidan <87931164+mostafaghidan@users.noreply.github.com>
2023-01-04 22:28:27 +0200

    1 binaries uploaded

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // run `node index.js` in the terminal
 
 console.log(`Hello Node.js v${process.versions.node}!`);
+const express = require("express")
+const app = express()
+app.listen(3000,console.log("sserver running on 3000"))
